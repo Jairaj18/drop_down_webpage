b8653420cffec126acdc2dc0a1fe753289af4ba6
Jairaj18 <109075330+Jairaj18@users.noreply.github.com>
2023-12-04 00:27:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+//import React,{useState} from 'react'
+import React,{useState} from 'react';
+import Select from 'react-dropdown-select';
 
-function App() {
+export default function App() {
+  const [value,setValue] = useState(null)
+
+  const option =[
+    {value:"Chocolate",label:"chocolate"},
+    {value:"CANDY",label:"candy"},
+    {value:"ICE CREAM",label:"Ice cream"},
+    {value:"BURGER",label:"Burger"},
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{margin:20, width:200}}>
+      
+      <Select 
+      options={option} 
+      defaultValue={value} 
+      placeholder="Select your Snack" 
+      onChange={setValue}
+      multi
+      isSearchable
+      
+      />
     </div>
-  );
-}
-
-export default App;
+  )
+} 
